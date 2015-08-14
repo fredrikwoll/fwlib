1bdcc68c39df8257010b931fc61a5566dd9c041f
folkibits <frewol@frewol.local>
2015-08-14 19:30:10 +0200

    Setting up

---
+++ fredrikwoll-lib.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Fredrik Woll WP Libary
+ * Plugin URI: http://fredrikwoll.no
+ * Description: This plugin adds extra functions and mekanisms to enhance my themes.
+ * Version: 1.0.0
+ * Author: Fredrik Woll Design
+ * Author URI: http://fredrikwoll.no
+ * License: GPL2
+ */
---
+++ updater.php
@@ -0,0 +1,27 @@
+<?php
+
+
+fwlib_updater { 
+	
+	protected $file;
+	protected $plugin;
+	protected $basename;
+	protected $active;
+
+
+	public function __construct( $file ) {
+		$this->file = $file;
+		add_action( 'admin_init', array( $this, 'set_plugin_properties' ) );
+		return $this;
+	}
+
+	public function set_plugin_properties() {
+		$this->plugin = get_plugin_data( $this->file );
+		$this->plugin = get_plugin_data( $this->file );
+		$this->plugin = is_plugin_active( $this->basename );
+
+
+	}
+
+
+}
