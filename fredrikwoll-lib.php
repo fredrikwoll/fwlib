7b595a12a5132596873b688e5cf3da924ccf2610
folkibits <frewol@frewol.local>
2015-08-14 20:43:53 +0200

    Updater ready
    http://www.smashingmagazine.com/2015/08/deploy-wordpress-plugins-with-gi
    thub-using-transients/

--- fredrikwoll-lib.php
+++ fredrikwoll-lib.php
@@ -3,8 +3,16 @@
  * Plugin Name: Fredrik Woll WP Libary
  * Plugin URI: http://fredrikwoll.no
  * Description: This plugin adds extra functions and mekanisms to enhance my themes.
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author: Fredrik Woll Design
  * Author URI: http://fredrikwoll.no
  * License: GPL2
  */
+
+//Include our update file
+include_once( plugin_dir_path( __FILE__ ) . 'updater.php');
+
+$updater = new fwlib_updater( __FILE__ ); // instantiate our class
+$updater->set_username( 'fredrikwoll' ); // Set user
+$updater->set_repository( 'fwlib' ); // set repo
+$updater->initialize(); // initialize the updater
