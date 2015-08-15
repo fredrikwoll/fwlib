7fb75a635dcba5a987867d604d72be142e5c34ef
folkibits <frewol@frewol.local>
2015-08-15 14:56:51 +0200

    changed version number

--- fredrikwoll-lib.php
+++ fredrikwoll-lib.php
@@ -3,7 +3,7 @@
  * Plugin Name: Fredrik Woll WP Libary
  * Plugin URI: http://fredrikwoll.no
  * Description: This plugin adds extra functions and mekanisms to enhance my themes.
- * Version: 1.0.2
+ * Version: 1.0.3
  * Author: Fredrik Woll Design
  * Author URI: http://fredrikwoll.no
  * License: GPL2
