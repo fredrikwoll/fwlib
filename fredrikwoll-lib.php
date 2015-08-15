6cb16967ac764024441549c322420952b91d03e7
folkibits <frewol@frewol.local>
2015-08-15 10:07:18 +0200

    updater
    Updated code so it works

--- fredrikwoll-lib.php
+++ fredrikwoll-lib.php
@@ -3,7 +3,7 @@
  * Plugin Name: Fredrik Woll WP Libary
  * Plugin URI: http://fredrikwoll.no
  * Description: This plugin adds extra functions and mekanisms to enhance my themes.
- * Version: 1.0.1
+ * Version: 1.0.2
  * Author: Fredrik Woll Design
  * Author URI: http://fredrikwoll.no
  * License: GPL2
@@ -16,3 +16,7 @@ $updater = new fwlib_updater( __FILE__ ); // instantiate our class
 $updater->set_username( 'fredrikwoll' ); // Set user
 $updater->set_repository( 'fwlib' ); // set repo
 $updater->initialize(); // initialize the updater
+
+/* 
+	$updater->authorize( 'abcdefghijk1234567890' ); // Your auth code goes here for private repos
+*/
